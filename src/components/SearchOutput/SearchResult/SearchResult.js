14b90d4e992ba823fdb0aadf676be23d40b2d111
mjfre <mjfre@aol.com>
2021-04-12 17:56:07 -0700

    Updating to fit Mongoose Search result object

--- src/components/SearchOutput/SearchResult/SearchResult.js
+++ src/components/SearchOutput/SearchResult/SearchResult.js
@@ -4,17 +4,20 @@ import classes from './SearchResult.module.css';
 const SearchResult = (props) => {
     console.log(props.term);
     let arrivalTime = props.arrival.scheduled || props.arrival.estimated || props.arrival.actual
-    arrivalTime = arrivalTime.substring(arrivalTime.indexOf('T')+1, arrivalTime.indexOf('+'));
+    arrivalTime = arrivalTime.substring(arrivalTime.indexOf('T') + 1, arrivalTime.indexOf('+'));
     return (
         <article className={classes.Result}>
-            <p> <span style={{fontSize:'x-large', fontWeight:'bold'}}>{props.airline.name} flight {props.flight.number} ({props.flight.iata})</span> <br />
-            <strong>Date:</strong> {props.flightDate} <br/>
+            <p><span style={{
+                fontSize: 'x-large',
+                fontWeight: 'bold'
+            }}>{props.airline.name} flight {props.flight.number} ({props.flight.iata})</span> <br/>
+                <strong>Date:</strong> {props.flightDate} <br/>
                 <strong>Flight Status:</strong> {props.flightStatus}</p>
             <p><strong>Departing:</strong> {props.departure.airport} ({props.departure.iata})</p>
             <p><strong>Arriving:</strong> {props.arrival.airport} ({props.arrival.iata}) <br/>
-                <strong>Arrival Time:</strong> {arrivalTime} <br />
+                <strong>Arrival Time:</strong> {arrivalTime} <br/>
                 <strong>Arrival Gate:</strong> Terminal {props.arrival.terminal} Gate {props.arrival.gate}
-                </p>
+            </p>
 
         </article>
     );
